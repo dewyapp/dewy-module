e4937c4340c5788415777fd54897bd931e3641fe
dmcb <damcburn@ucalgary.ca>
2015-09-24 17:41:54 -0600

    Fixes DRUPAL_ROOT definition to be fool-proof
    DRUPAL_ROOT was defined by the module location, but the module location
    could potentially be a link that isn’t within the sites directory,
    which would make the logic fail. The POST now passes DRUPAL_ROOT from
    Drupal itself.

--- php/dewey_counter.php
+++ php/dewey_counter.php
@@ -8,20 +8,22 @@
 /**
 * Root directory of Drupal installation.
 */
-define('DRUPAL_ROOT', substr($_SERVER['SCRIPT_FILENAME'], 0, strpos($_SERVER['SCRIPT_FILENAME'], 'sites')));
-// Change the directory to the Drupal root.
-chdir(DRUPAL_ROOT);
+if (isset($_POST['drupal'])) {
+  define('DRUPAL_ROOT', $_POST['drupal']);
+  // Change the directory to the Drupal root.
+  chdir($_POST['drupal']);
 
-include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_VARIABLES);
-$path = '/';
-if (isset($_POST['path'])) {
-  $path = $_POST['path'];
+  include_once $_POST['drupal'] . '/includes/bootstrap.inc';
+  drupal_bootstrap(DRUPAL_BOOTSTRAP_VARIABLES);
+  $path = '/';
+  if (isset($_POST['path'])) {
+    $path = $_POST['path'];
+  }
+  db_merge('dewey_counter')
+    ->key(array('path' => $path))
+    ->fields(array(
+      'count' => 1,
+    ))
+    ->expression('count', 'count + 1')
+    ->execute();
 }
-db_merge('dewey_counter')
-  ->key(array('path' => $path))
-  ->fields(array(
-    'count' => 1,
-  ))
-  ->expression('count', 'count + 1')
-  ->execute();
